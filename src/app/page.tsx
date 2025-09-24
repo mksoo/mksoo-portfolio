97d958bf0d4895a040dbc31f2ce2ace0d0f29bf1
mksoo <mgs3067@naver.com>
2025-09-24 11:34:51 +0900

    refactor(page): 홈 페이지에 신규 섹션 통합

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Hero from "@/app/components/Hero";
+import Skills from "@/app/components/Skills";
+import Career from "@/app/components/Career";
+import Projects from "@/app/components/Projects";
+import Contact from "@/app/components/Contact";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Hero />
+      <Skills />
+      <Career />
+      <Projects />
+      <Contact />
     </div>
   );
 }
